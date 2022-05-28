d43f7e0f588ab9e93d8782bf577183f7f514f4ef
Jaqueline21623 <jaqueline.huahuaccapa@upeu.edu.pe>
2022-05-28 20:50:43 +0000

    jaqueline

---
+++ Tarearep/ejercicio123.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void imprimeif_1(String[] args) {
+        Scanner obtener = new Scanner(System.in);
+        System.out.println("ingrese un numero: ");
+        int numero = obtener.nextInt();
+        if (numero > 999 & numero > 100000){
+            int a = numero / 1000;
+            int b = numero - (a*1000);
+            int c = b / 100;
+            int d = b - (c*100);
+            int e = d / 10;
+            int f = d - (e*10);
+            int g = (f*1000)+(e*100)+(c*10)+0;
+            System.out.println("*\n");
+        }System.out.println("imprime if: "); imprimeif_1; ;
+    }
+}
