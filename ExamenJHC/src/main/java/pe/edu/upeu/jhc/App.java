a3615025da9fecc711c581eed5442f93e59fb362
jaqueline21623 <jaqueline.huahuaccpa@upeu.edu.pe>
2022-06-28 08:44:54 -0500

    Ventas

--- ExamenJHC/src/main/java/pe/edu/upeu/jhc/App.java
+++ ExamenJHC/src/main/java/pe/edu/upeu/jhc/App.java
@@ -1,13 +1,71 @@
 package pe.edu.upeu.jhc;
 
 /**
- * Hello world!
- *
+ * App
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+    
+}
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class LeerTeclado {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+public int leer(int valor, String msg){  
+  System.out.print(msg+": ");
+  try{
+    valor=Integer.parseInt(br.readLine());
+  }catch(Exception e){
+    valor=leer(valor, msg);
+  }
+  System.out.println();
+  return valor;
+}
+
+public long leer(long valor, String msg){  
+  System.out.print(msg+": ");
+  try{
+    valor=Long.parseLong(br.readLine());
+  }catch(Exception e){
+    valor=leer(valor, msg);
+  }
+  System.out.println();
+  return valor;
+}
+
+public double leer(double valor, String msg){  
+  System.out.print(msg+": ");
+  try{
+    valor=Double.parseDouble(br.readLine());
+  }catch(Exception e){
+    valor=leer(valor, msg);
+  }
+  System.out.println();
+  return valor;
+}
+
+public char leer(char valor, String msg){ 
+  System.out.print(msg+": ");
+  try{
+    valor=br.readLine().charAt(0);//
+  }catch(Exception e){
+    valor=leer(valor, msg);
+  }
+  System.out.println();
+  return valor;
+}
+
+public String leer(String valor, String msg){  
+  System.out.print(msg+": ");
+  try{
+    valor=br.readLine();//
+  }catch(Exception e){
+    valor=leer(valor, msg);
+  }
+  System.out.println();
+  return valor;
+}
 }
